d7dbdb2a89e759864802aa53351a9cc8acc77086
FabrizioAtiquipa <fabrizio.atiquipa@tecsup.edu.pe>
2021-11-26 21:57:55 -0500

    lab15 Finalizado-1

--- Lab15/App_Start/RouteConfig.cs
+++ Lab15/App_Start/RouteConfig.cs
@@ -16,8 +16,7 @@ namespace Lab15
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Categories", action = "Index", 
-                id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- Lab15/Models/GetStudentGrades_Result.cs
+++ Lab15/Models/GetStudentGrades_Result.cs
@@ -7,17 +7,13 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Lab15
+namespace Lab15.Models
 {
     using System;
-    using System.Collections.Generic;
     
-    public partial class sysdiagrams
+    public partial class GetStudentGrades_Result
     {
-        public string name { get; set; }
-        public int principal_id { get; set; }
-        public int diagram_id { get; set; }
-        public Nullable<int> version { get; set; }
-        public byte[] definition { get; set; }
+        public int EnrollmentID { get; set; }
+        public Nullable<decimal> Grade { get; set; }
     }
 }
